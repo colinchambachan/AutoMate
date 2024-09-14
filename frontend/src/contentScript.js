7244b7c3c73ff456ec12ecd62e379c07636474d6
afahimi <amin.f1000@gmail.com>
2024-09-14 12:20:42 -0700

    modularity

--- frontend/src/contentScript.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "extensionInstalled") {
-    console.log("Extension Installed message received in content script");
-    // Perform actions based on this event in the content script context
-  }
-
-  return true;
-});
