4de2539f0a6437ac3f3e76852ce26ba31d07ba22
afahimi <amin.f1000@gmail.com>
2024-09-14 09:08:04 -0700

    background worker works

--- frontend/public/background.js
+++ frontend/public/background.js
@@ -3,5 +3,15 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log("Message received from popup.");
+  console.log("Message received from popup:", request);
+
+  if (request.action === "testFunctionality") {
+    console.log("Executing test functionality");
+
+    // Send a response back to the popup
+    sendResponse({ status: "Functionality tested." });
+  }
+
+  // Return true to indicate you want to send an asynchronous response
+  return true;
 });
