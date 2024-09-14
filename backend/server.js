dd466a3a7ae1f639295dfb3a302bd86aaa0c8442
Jerry Sun <sunyiran0817@gmail.com>
2024-09-14 02:26:30 -0400

    integrate cohere to backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log("Server running");
+const axios = require("axios");
+const express = require("express");
+const { CohereClient } = require("cohere-ai");
+// Import the API key from the .env file
+require("dotenv").config();
+const apiKey = process.env.COHERE_API_KEY;
+
+const cohere = new CohereClient({
+  token: apiKey,
+});
+
+const app = express();
+
+app.listen(8000, async () => {
+  let respone = await cohere.checkApiKey();
+
+  console.log(respone);
+
+  console.log(`Server is running on port 8000.`);
+
+  response = await await cohere.chat({
+    message: "hello world!",
+  });
+  console.log(response);
+});
